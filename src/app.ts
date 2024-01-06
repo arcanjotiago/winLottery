d01c251ee53f82ffdaedbf0051d9e02f6a581744
arcanjotiago <tiagoarcanjom@gmail.com>
2024-01-06 19:20:39 -0300

    bug fix

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,15 @@ app.listen(port, () => {
 
 app.post('/generatenumbers', (req: Request, res: Response) => {
     const quantity = req.body.quantity;
-    const listNumbers = new GenerateNumber(quantity);
-    const responseWinLottery = new ResponseWin(`You winLottery numbers is:  ${listNumbers.genListNumbers()}`, 200)
+    let listNumbers;
+    let responseWinLottery;
 
+    if (quantity > 60 || quantity < 1){
+      responseWinLottery = new ResponseWin(`The quantity entered is invalid. Please enter a number greater than or equal to 1 and less than or equal to 60`, 400);
+      return res.json(responseWinLottery)
+    }
+    
+    listNumbers = new GenerateNumber(quantity);
+    responseWinLottery = new ResponseWin(`You winLottery numbers is:  ${listNumbers.genListNumbers()}`, 200);
     res.json(responseWinLottery)
-  
       })
